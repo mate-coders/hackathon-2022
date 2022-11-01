1bdc94671353bd6c41e45e54bd8fdb1625ccbab8
agustin1996ra <rodriguezalvarezagustin@gmail.com>
2022-10-31 23:50:16 -0300

    analisis y graficos

---
+++ bibliografia/datos_indec/eho/analisis.R
@@ -0,0 +1,21 @@
+library(tidyr)
+library(readxl)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+demanda <- read_csv("demanda.csv", col_types = 'Dnnnnnnnnn')
+ofertaydemanda <- read_csv("demandayoferta.csv")
+
+i <- ggplot(demanda, aes(date, PT))
+i + geom_line()
+
+i + geom_col()
+
+i + geom_area()
+
+p <- ggplot(ofertaydemanda, aes(date, POPT))
+p + geom_area()
+
+d <- ggplot(demanda, aes(date,DT))
+d + geom_area()
--- bibliografia/datos_indec/eho/limpieza.R
+++ bibliografia/datos_indec/eho/limpieza.R
@@ -1,16 +1,17 @@
 library(tidyr)
 library(readxl)
 library(dplyr)
+library(tidyverse)
 fecha <- function(meses, anio){
   x <- 1
   for (mes in meses){
-    meses[x] <- paste(mes, anio, sep = "-")
+    meses[x] <- paste(anio,mes,"01", sep = "-")
     x <- x + 1
   }
   return (meses)
 }
 
-
+nmeses <- c("01","02","03","04","05","06","07","08","09","10","11","12")
 # EOH demanda 18
 columnas <- c('PT','PR','PNR','VT','VR','VNR','DT','DR','DNR')
 meses <- c('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre')
@@ -18,7 +19,7 @@ p <- read_excel('EOH_cuadros_18/EOH_dic18demanda_cuadros.xls', range = 'San Rafa
 p2 <- select(p, meses)
 p3 <- drop_na(p2)
 p4 <- t(p3)
-filas <- fecha(meses, "18")
+filas <- fecha(nmeses, "2018")
 eoh18 <- data.frame(`colnames<-`(p4, columnas), row.names = filas)
 
 # EOH demanda 19
@@ -26,7 +27,7 @@ p <- read_excel('EOH_cuadros_19/EOH_dic19demanda_cuadros.xls', range = 'San Rafa
 p2 <- select(p, meses)
 p3 <- drop_na(p2)
 p4 <- t(p3)
-filas <- fecha(meses, "19")
+filas <- fecha(nmeses, "2019")
 eoh19 <- data.frame(`colnames<-`(p4, columnas), row.names = filas)
 
 # EOH demanda 20
@@ -34,7 +35,7 @@ p <- read_excel('EOH_cuadros_20/eoh_dic20_demanda_cuadros.xls', range = 'San Raf
 p2 <- select(p, meses)
 p3 <- slice(p2, c(2:4, 6:8, 10:12))
 p4 <- t(p3)
-filas <- fecha(meses, "20")
+filas <- fecha(nmeses, "2020")
 eoh20 <- data.frame(`colnames<-`(p4, columnas), row.names = filas)
 
 # EOH demanda 21
@@ -42,7 +43,7 @@ p <- read_excel('EOH_cuadros_21/eoh_dic21_demanda_cuadros.xls', range = 'San Raf
 p2 <- select(p, meses)
 p3 <- slice(p2, c(2:4, 6:8, 10:12))
 p4 <- t(p3)
-filas <- fecha(meses, "21")
+filas <- fecha(nmeses, "2021")
 eoh21 <- data.frame(`colnames<-`(p4, columnas), row.names = filas)
 
 # EOH demanda 22
@@ -50,10 +51,11 @@ p <- read_excel('EOH_cuadros_21/eoh_dic21_demanda_cuadros.xls', range = 'San Raf
 p2 <- select(p, meses[1:8])
 p3 <- slice(p2, c(2:4, 6:8, 10:12))
 p4 <- t(p3)
-filas <- fecha(meses[1:8], "22")
+filas <- fecha(nmeses[1:8], "2022")
 eoh22 <- data.frame(`colnames<-`(p4, columnas), row.names = filas)
 
 tabla_demanda <- bind_rows(eoh18,eoh19,eoh20,eoh21,eoh22)
+tabla_demanda <- rownames_to_column(tabla_demanda, var = "date")
 
 
 columnas <- c("ET", "EH", "EPH", "HUDT", "HUDH", "HUDPH", "HUOT", "HUOH", "HUOPH", "PDT", "PDH", "PDPH", "POT", "POH", "POPH", "POHUT", "POHUH", "POHUPH", "POPT", "POPH", "POPPH", "VT", "VH", "VPH", "DET", "DEH", "DEPH")
@@ -62,7 +64,7 @@ p <- read_excel('EOH_cuadros_18/EOH_dic18ofyde_establecimientos.xls', range = 'S
 p <- select(p, meses)
 p <- drop_na(p)
 p <- t(p)
-filas <- fecha(meses, "18")
+filas <- fecha(nmeses, "2018")
 eoh_oyd_18 = data.frame(`colnames<-`(p, columnas), row.names = filas)
 
 # EOH oferta y demanda 19
@@ -70,7 +72,7 @@ p <- read_excel('EOH_cuadros_19/EOH_dic19ofyde_establecimientos.xls', range = 'S
 p <- select(p, meses)
 p <- drop_na(p)
 p <- t(p)
-filas <- fecha(meses, "19")
+filas <- fecha(nmeses, "2019")
 eoh_oyd_19 = data.frame(`colnames<-`(p, columnas), row.names = filas)
 
 # EOH oferta y demanda 20
@@ -78,7 +80,7 @@ p <- read_excel('EOH_cuadros_20/eoh_dic20_oyd_cuadros.xls', range = 'San Rafael!
 p <- select(p, meses)
 p <- slice(p, c(3:5, 8:10, 13:15, 18:20, 23:25, 28:30, 33:35, 38:40, 43:45))
 p <- t(p)
-filas <- fecha(meses, "20")
+filas <- fecha(nmeses, "2020")
 eoh_oyd_20 = data.frame(`colnames<-`(p, columnas), row.names = filas)
 
 # EOH oferta y demanda 21
@@ -86,7 +88,7 @@ p <- read_excel('EOH_cuadros_21/eoh_dic21_oyd_cuadros.xls', range = 'San Rafael!
 p <- select(p, meses)
 p <- slice(p, c(3:5, 8:10, 13:15, 18:20, 23:25, 28:30, 33:35, 38:40, 43:45))
 p <- t(p)
-filas <- fecha(meses, "21")
+filas <- fecha(nmeses, "2021")
 eoh_oyd_21 = data.frame(`colnames<-`(p, columnas), row.names = filas)
 
 # EOH oferta y demanda 22
@@ -94,11 +96,12 @@ p <- read_excel('EOH_cuadros_22/eoh_ago22_oyd_cuadros.xls', range = 'San Rafael!
 p <- select(p, meses[1:8])
 p <- slice(p, c(3:5, 8:10, 13:15, 18:20, 23:25, 28:30, 33:35, 38:40, 43:45))
 p <- t(p)
-filas <- fecha(meses[1:8], "22")
+filas <- fecha(nmeses[1:8], "2022")
 eoh_oyd_22 = data.frame(`colnames<-`(p, columnas), row.names = filas)
 
 tabla_oyd = bind_rows(eoh_oyd_18, eoh_oyd_19, eoh_oyd_20, eoh_oyd_21, eoh_oyd_22)
+tabla_oyd <- rownames_to_column(tabla_oyd, var = "date")
 
-# Graficos
-
+write_csv(tabla_demanda, "demanda.csv")
+write_csv(tabla_oyd, "demandayoferta.csv")
 
