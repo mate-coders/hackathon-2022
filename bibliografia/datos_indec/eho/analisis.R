6fb9cb1ad049d98d8628215e60286869570beea0
agustin1996ra <rodriguezalvarezagustin@gmail.com>
2022-11-16 00:31:35 -0300

    Graficos de torta

--- bibliografia/datos_indec/eho/analisis.R
+++ bibliografia/datos_indec/eho/analisis.R
@@ -21,3 +21,15 @@ p + geom_area()
 d <- ggplot(demanda, aes(date,DT))
 d + geom_smooth(method = ) +
   theme_minimal()
+
+# Plazas Provincia de Mendoza
+plazas_pm <- c(14100, 9009, 6198, 3137, 2727, 2276, 5533)
+n_plazas_pm <- c("Hoteles", "Campings", "Cabañas", "PAT", "Apart Hotel", "Hostel", "Otros")
+pie(plazas_pm, labels = n_plazas_pm, main = "Plazas disponibles prov. Mendoza")
+
+# Plazas dpto San Rafael
+plazas_sr <- c(2540, 2376, 1640, 1017, 2452)
+n_plazas_sr <- c("Cabañas", "Campings", "Hoteles", "PAT", "Otros")
+pie(plazas_sr, labels = n_plazas_sr, main = "Plazas disponibles Dpto. San Rafael") 
+
+
