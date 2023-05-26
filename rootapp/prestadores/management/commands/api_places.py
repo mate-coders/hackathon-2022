924dea1e8fe8d81b4683a71c02c06c7287ce7b90
Agustin   Rodriguez Alvarez <63318331+agustin1996ra@users.noreply.github.com>
2023-05-26 20:48:28 -0300

    corección api_places

--- rootapp/prestadores/management/commands/api_places.py
+++ rootapp/prestadores/management/commands/api_places.py
@@ -2,7 +2,9 @@ from django.core.management.base import BaseCommand, CommandParser
 import requests, json
 from ...models import Place, Type
 from datetime import datetime
-# Esta función nos las dio chatGPT para nutrir la base de datos con información de lugares turisticos
+
+
+# Esta función nos las dio chatGPT para nutrir la base de datos con información de lugares turísticos
 class Command(BaseCommand):
     help = 'Obtiene lugares turísticos en San Rafael, Mendoza utilizando la API Place Search de Google Maps y los almacena en una base de datos'
 
@@ -11,21 +13,31 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Iniciando búsqueda de lugares turísticos...')
+        
         key = 'AIzaSyAl7kuTXJAepiuX25-GR4H2b9vplv_HT9c'
-        busqueda = 'Lugares turisticos en san rafael'
+        busqueda = options['busqueda']
         busqueda_formateada = busqueda.replace(' ', '+')
         url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+ busqueda_formateada +'&key=' + key
         response = requests.get(url)
         data = json.loads(response.text)
+        
         for result in data['results']:
-            place = Place(name=result['name'], address=result['formatted_address'], latitude=result['geometry']['location']['lat'], longitude=result['geometry']['location']['lng'], rating = result['rating'], users_rating = result['user_ratings_total'])
+            
+            ex = Place.objects.filter(code=result['plus_code']['global_code']).exists()
+            if ex:                    
+                continue
+            
+            place = Place(name=result['name'], address=result['formatted_address'], latitude=result['geometry']['location']['lat'], longitude=result['geometry']['location']['lng'], rating = result['rating'], users_rating = result['user_ratings_total'], code = result['plus_code']['global_code'])
             place.save()
+            
             for tipo in result['types']:
+                
                 existe = Type.objects.filter(name=tipo).exists()
                 if existe:
                     o_type = Type.objects.get(name=tipo)
                     place.type.add(o_type)
                     continue
+                
                 o_type = Type(name=tipo)
                 o_type.save()
                 place.type.add(o_type)
@@ -34,7 +46,9 @@ class Command(BaseCommand):
         
             self.stdout.write('Lugar turístico guardado: ' + place.name)
         try:
-            with open('query.json', 'w') as archivo:
+            fecha = datetime.now()
+            fecha_fromateada = fecha.strftime("%d-%m-%Y %H-%M")
+            with open(f'./querys/query_{busqueda}_{fecha_fromateada}.json', 'w') as archivo:
                 json.dump(data, archivo, indent=4)
         except Exception as e:
             print(e)
--- rootapp/prestadores/models.py
+++ rootapp/prestadores/models.py
@@ -10,6 +10,7 @@ class Place(models.Model):
     longitude = models.FloatField()
     rating = models.FloatField()
     users_rating = models.IntegerField()
+    code = models.CharField(max_length=25)
     type = models.ManyToManyField(Type)
 
 """
