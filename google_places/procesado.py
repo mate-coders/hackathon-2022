bd9d5e5d744e9fa012473ac8823ba82286055467
agustin1996ra <rodriguezalvarezagustin@gmail.com>
2023-05-25 22:33:12 -0300

    Puebas de la api e implementación

--- google_places/procesado.py
+++ google_places/procesado.py
@@ -1,10 +1,11 @@
 import json
 try:
-    with open('consulta_places.json', 'r') as archivo:
+    with open('google_places/consulta_places.json', 'r') as archivo:
         content = archivo.read()
         data = json.loads(content)
         for result in data['results']:
             print(result['name'])
+            print(result['geometry']['location'])
     
         
 except Exception as e:
