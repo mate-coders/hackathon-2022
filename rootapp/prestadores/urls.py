cdec40e6abbfdb6e3a1af9500750a653a1730fe3
Martin Torres <martinalejandrotorres77@gmail.com>
2023-05-26 01:53:03 -0300

    cargo pagina de prueba
    Esta en una pagina de prueba el contenido hay que modificar

--- rootapp/prestadores/urls.py
+++ rootapp/prestadores/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('prueba/', views.prueba, name='prueba'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('typography/', views.typography, name='typography')
 ]
 
--- rootapp/prestadores/views.py
+++ rootapp/prestadores/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def prueba(request):
     return render(request, 'prueba.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def contact(request):
+    return render(request, 'contact-us.html', {})
+
+def typography(request):
+    return render(request, 'typography.html', {})
